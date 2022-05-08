f9140d175553cf46b7672739bb767d6ddbc3e2e2
durveshramane26 <durveshramane6@gmail.com>
2022-05-08 13:04:42 +0530

    fix: prev button

--- kaali.js
+++ kaali.js
@@ -29,6 +29,101 @@ if (hamburgericon != null ) {
     });
 }
 
+
+//Carousel
+const track = document.querySelector(".carousel-track");
+
+const slides = Array.from(track?.children || []);
+console.log({slides});
+
+const nextButton = document.querySelector(".carousel-button-right");
+const prevButton = document.querySelector(".carousel-button-left");
+
+const dotsNav = document.querySelector(".carousel-nav");
+const dots = Array.from(dotsNav?.children || []);
+
+const slideSize = slides[0]?.getBoundingClientRect();
+
+const slideWidth = slideSize?.width;
+
+const setSlidePosition = (slide, index) => {
+    slide.style.left = slideWidth * index + "px";
+};
+slides.forEach(setSlidePosition);
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+    const amountToMove = targetSlide.style.left;
+    track.style.transform = "translateX(-" + amountToMove + ")";
+    currentSlide.classList.remove("current-slide");
+    targetSlide.classList.add("current-slide");
+};
+
+const updateDots = (currentDot, targetDot) => {
+    currentDot.classList.remove("current-slide");
+    targetDot.classList.add("current-slide");
+};
+
+const toggleArrows = (targetIndex, prevButton, nextButton, slides) => {
+    if (targetIndex === 0) {
+        prevButton.classList.add("hide");
+        nextButton.classList.remove("hide");
+    }
+    else if (targetIndex === slides.length - 1) {
+        nextButton.classList.add("hide");
+        prevButton.classList.remove("hide");
+    }
+    else {
+        nextButton.classList.remove("hide");
+        prevButton.classList.remove("hide");
+    }
+};
+
+nextButton?.addEventListener("click", () => {
+    const currentSlide = track.querySelector(".current-slide");
+    const nextSlide = currentSlide.nextElementSibling;
+    if (nextSlide !== null) {
+        moveToSlide(track, currentSlide, nextSlide);
+        const currentDot = dotsNav.querySelector(".current-slide");
+        const nextDot = currentDot.nextElementSibling;
+        updateDots(currentDot, nextDot);
+        const targetIndex = dots.findIndex((dot) => nextDot === dot);
+        toggleArrows(targetIndex, prevButton, nextButton, slides);
+    }
+});
+
+prevButton?.addEventListener("click", () => {
+    const currentSlide = track.querySelector(".current-slide");
+    const prevSlide = currentSlide.previousElementSibling;
+
+    if (prevSlide !== null) {
+        moveToSlide(track , currentSlide, prevSlide);
+        const currentDot = dotsNav.querySelector(".current-slide");
+        const prevDot = currentDot.previousElementSibling;
+        updateDots(currentDot, prevDot);
+
+        const targetIndex = dots.findIndex((dot) => prevDot === dot);
+        toggleArrows(targetIndex, prevButton, nextButton, slides);
+    };
+});
+
+dotsNav?.addEventListener("click", (event) => {
+    const currentSlide = track.querySelector(".current-slide");
+    const currentDot = dotsNav.querySelector(".current-slide");
+    const targetDot = event.target.closest("button");
+    //on complete dotsNav where ever I click except button (specific dots) it will return null.
+
+    const targetIndex = dots.findIndex((dot) => dot ===  targetDot);
+    const targetSlide = slides[targetIndex];
+    moveToSlide(track, currentSlide, targetSlide);
+    updateDots(currentDot, targetDot);
+    toggleArrows(targetIndex, prevButton, nextButton, slides);
+});
+
+
+
+
+
+//---------------------------------------------------------------
 // dark mode
 
 function removeClassNameFromElement(element, className) {
@@ -49,7 +144,7 @@ function setItemtoLocalStorage(item, value) {
 
 
 const toggleMode = document.querySelectorAll(".btn.btn-toggle#theme-toggler");
-console.log({toggleMode})
+//console.log({toggleMode})
 const mainBodyKaali = document.querySelector(".body-style-development-purpose");
 const darkIcons = document.querySelectorAll("#dark-icon");
 const currentTheme = localStorage.getItem("theme");
